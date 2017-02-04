c8d345b244038c383f4b8fd5edc3afc58e6545b6
GrizzlyGames <info@grizzlygames.co>
2017-02-03 19:47:54 -0500

    Further Improvements, Working on Game_Manager

--- Creep-Meter_1.0/CreepMeterV2.0/Assets/GAME_MANAGER_SCRIPT.cs
+++ Creep-Meter_1.0/CreepMeterV2.0/Assets/GAME_MANAGER_SCRIPT.cs
@@ -5,14 +5,48 @@ using UnityEngine.UI;
 public class GAME_MANAGER_SCRIPT : MonoBehaviour
 {
     public static GAME_MANAGER_SCRIPT instance;
-
+    
     public bool herProfile_bool;
-
     public string[] herQuestions_Str_Ary;
     public string[] herAnswer1_Str_Ary;
     public string[] herAnswer2_Str_Ary;
     public string[] herAnswer3_Str_Ary;
     public string[] herAnswer4_Str_Ary;
+
+    public bool hisProfile_bool;
+    public string[] hisProfileQuestions_Str_Ary;
+    public string[] hisProfileAnswer1_Str_Ary;
+    public string[] hisProfileAnswer2_Str_Ary;
+    public string[] hisProfileAnswer3_Str_Ary;
+    public string[] hisProfileAnswer4_Str_Ary;
+
+    public bool hisCharacter_bool;
+    public string[] hisCharacterQuestions_Str_Ary;
+    public string[] hisCharacterAnswer1_Str_Ary;
+    public string[] hisCharacterAnswer2_Str_Ary;
+    public string[] hisCharacterAnswer3_Str_Ary;
+    public string[] hisCharacterAnswer4_Str_Ary;
+
+    public bool hisPhysicalHealth_bool;
+    public string[] hisPhysicalHealthQuestions_Str_Ary;
+    public string[] hisPhysicalHealthAnswer1_Str_Ary;
+    public string[] hisPhysicalHealthAnswer2_Str_Ary;
+    public string[] hisPhysicalHealthAnswer3_Str_Ary;
+    public string[] hisPhysicalHealthAnswer4_Str_Ary;
+
+    public bool hisMentalHealth_bool;
+    public string[] hisMentalHealthQuestions_Str_Ary;
+    public string[] hisMentalHealthAnswer1_Str_Ary;
+    public string[] hisMentalHealthAnswer2_Str_Ary;
+    public string[] hisMentalHealthAnswer3_Str_Ary;
+    public string[] hisMentalHealthAnswer4_Str_Ary;
+
+    public bool hisHistory_bool;
+    public string[] hisHistoryQuestions_Str_Ary;
+    public string[] hisHistoryAnswer1_Str_Ary;
+    public string[] hisHistoryAnswer2_Str_Ary;
+    public string[] hisHistoryAnswer3_Str_Ary;
+    public string[] hisHistoryAnswer4_Str_Ary;
 
     public GameObject[] qustionButtons_Ary;  
 
@@ -37,12 +71,51 @@ public class GAME_MANAGER_SCRIPT : MonoBehaviour
     void Start(){
         if (herProfile_bool)
         {
-            for (int i = 0; i < qustionButtons_Ary.Length; i++)
+            for (int i = 0; i < herQuestions_Str_Ary.Length; i++)
             {
                 qustionButtons_Ary[i].GetComponent<QUESTIONS_BUTTON_SCRIPT>().SetStrings(herQuestions_Str_Ary[i], herAnswer1_Str_Ary[i], herAnswer2_Str_Ary[i], herAnswer3_Str_Ary[i], herAnswer4_Str_Ary[i]);
                 qustionButtons_Ary[i].GetComponent<QUESTIONS_BUTTON_SCRIPT>().SetButtonText();
             }                
         }
+        if (hisProfile_bool)
+        {
+            for (int i = 0; i < hisCharacterQuestions_Str_Ary.Length; i++)
+            {
+                qustionButtons_Ary[i].GetComponent<QUESTIONS_BUTTON_SCRIPT>().SetStrings(hisProfileQuestions_Str_Ary[i], hisProfileAnswer1_Str_Ary[i], hisProfileAnswer2_Str_Ary[i], hisProfileAnswer3_Str_Ary[i], hisProfileAnswer4_Str_Ary[i]);
+                qustionButtons_Ary[i].GetComponent<QUESTIONS_BUTTON_SCRIPT>().SetButtonText();
+            }
+        }
+        if (hisCharacter_bool)
+        {
+            for (int i = 0; i < hisCharacterQuestions_Str_Ary.Length; i++)
+            {
+                qustionButtons_Ary[i].GetComponent<QUESTIONS_BUTTON_SCRIPT>().SetStrings(hisCharacterQuestions_Str_Ary[i], hisCharacterAnswer1_Str_Ary[i], hisCharacterAnswer2_Str_Ary[i], hisCharacterAnswer3_Str_Ary[i], hisCharacterAnswer4_Str_Ary[i]);
+                qustionButtons_Ary[i].GetComponent<QUESTIONS_BUTTON_SCRIPT>().SetButtonText();
+            }
+        }
+        if (hisPhysicalHealth_bool)
+        {
+            for (int i = 0; i < hisPhysicalHealthQuestions_Str_Ary.Length; i++)
+            {
+                qustionButtons_Ary[i].GetComponent<QUESTIONS_BUTTON_SCRIPT>().SetStrings(hisPhysicalHealthQuestions_Str_Ary[i], hisPhysicalHealthAnswer1_Str_Ary[i], hisPhysicalHealthAnswer2_Str_Ary[i], hisPhysicalHealthAnswer3_Str_Ary[i], hisPhysicalHealthAnswer4_Str_Ary[i]);
+                qustionButtons_Ary[i].GetComponent<QUESTIONS_BUTTON_SCRIPT>().SetButtonText();
+            }
+        }
+        if (hisMentalHealth_bool)
+        {
+            for (int i = 0; i < hisPhysicalHealthQuestions_Str_Ary.Length; i++)
+            {
+                qustionButtons_Ary[i].GetComponent<QUESTIONS_BUTTON_SCRIPT>().SetStrings(hisMentalHealthQuestions_Str_Ary[i], hisMentalHealthAnswer1_Str_Ary[i], hisMentalHealthAnswer2_Str_Ary[i], hisMentalHealthAnswer3_Str_Ary[i], hisMentalHealthAnswer4_Str_Ary[i]);
+                qustionButtons_Ary[i].GetComponent<QUESTIONS_BUTTON_SCRIPT>().SetButtonText();
+            }
+        }
+        if (hisHistory_bool)
+        {
+            for (int i = 0; i < hisPhysicalHealthQuestions_Str_Ary.Length; i++)
+            {
+                qustionButtons_Ary[i].GetComponent<QUESTIONS_BUTTON_SCRIPT>().SetStrings(hisHistoryQuestions_Str_Ary[i], hisHistoryAnswer1_Str_Ary[i], hisHistoryAnswer2_Str_Ary[i], hisHistoryAnswer3_Str_Ary[i], hisHistoryAnswer4_Str_Ary[i]);
+                qustionButtons_Ary[i].GetComponent<QUESTIONS_BUTTON_SCRIPT>().SetButtonText();
+            }
+        }
     }
-
 }
